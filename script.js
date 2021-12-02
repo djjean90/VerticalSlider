930f05a437a5266836970c2df5d7254e72e30b9f
djjean <djjean90@gmail.com>
2021-12-02 08:31:36 +0200

    final

--- script.js
+++ script.js
@@ -6,18 +6,29 @@ const downButton = document.querySelector(".down-button");
 const slideLength = slideRight.querySelectorAll("div").length;
 
 let activeSlideIndex = 0;
+console.log(slideRight);
 
 slideLeft.style.top = `-${(slideLength - 1) * 100}vh`;
 
-upButton.addEventListener("click", changeSlide("up"));
-downButton.addEventListener("click", changeSlide("down"));
+upButton.addEventListener("click", () => changeSlide("up"));
+downButton.addEventListener("click", () => changeSlide("down"));
 
-const changeSlide = (direction) => {
+function changeSlide(direction) {
   const slideHeight = sliderContainer.clientHeight;
   if (direction === "up") {
     activeSlideIndex++;
   }
   if (activeSlideIndex > slideLength - 1) {
     activeSlideIndex = 0;
+  } else if (direction === "down") {
+    activeSlideIndex--;
   }
-};
+  if (activeSlideIndex < 0) {
+    activeSlideIndex = slideLength - 1;
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * slideHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${activeSlideIndex * slideHeight}px)`;
+}
